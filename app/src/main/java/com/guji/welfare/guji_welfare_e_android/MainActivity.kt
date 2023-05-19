0275aeec8e104abc8930aa24d78bc0feeea247c0
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-19 10:56:23 +0900

    :wheelchair: add MainActivity DataBinding

--- app/src/main/java/com/guji/welfare/guji_welfare_e_android/MainActivity.kt
+++ app/src/main/java/com/guji/welfare/guji_welfare_e_android/MainActivity.kt
@@ -2,10 +2,15 @@ package com.guji.welfare.guji_welfare_e_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.guji.welfare.guji_welfare_e_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.main = this
     }
 }
