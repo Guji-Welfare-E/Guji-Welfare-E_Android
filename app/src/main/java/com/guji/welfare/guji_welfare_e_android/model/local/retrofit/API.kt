880218ddc276971e75c1d909bdecdbe2cb33b530
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-09-22 00:32:22 +0900

    :coffin: :: delete code

--- app/src/main/java/com/guji/welfare/guji_welfare_e_android/model/local/retrofit/API.kt
+++
@@ -1,4 +0,0 @@
-package com.guji.welfare.guji_welfare_e_android.model.local.retrofit
-
-interface API {
-}
--- app/src/main/java/com/guji/welfare/guji_welfare_e_android/viewmodel/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.guji.welfare.guji_welfare_e_android.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel:ViewModel() {
-
-}
