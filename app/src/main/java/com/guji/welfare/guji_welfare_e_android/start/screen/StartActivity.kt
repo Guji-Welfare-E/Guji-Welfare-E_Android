9c95cb05bd818a543e6522c769b3cd818709e6ac
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-09-22 00:30:29 +0900

    :sparkles: :: add splash screen

---
+++ app/src/main/java/com/guji/welfare/guji_welfare_e_android/start/screen/StartActivity.kt
@@ -0,0 +1,15 @@
+package com.guji.welfare.guji_welfare_e_android.start.screen
+
+import androidx.activity.viewModels
+import com.guji.welfare.guji_welfare_e_android.R
+import com.guji.welfare.guji_welfare_e_android.base.BaseActivity
+import com.guji.welfare.guji_welfare_e_android.databinding.ActivityMainBinding
+import com.guji.welfare.guji_welfare_e_android.start.viewmodel.StartViewModel
+
+class StartActivity : BaseActivity<ActivityMainBinding, StartViewModel>(R.layout.activity_start) {
+    override val viewModel: StartViewModel by viewModels()
+
+    override fun start() {
+
+    }
+}
