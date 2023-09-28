8b4842429e7d8dfb1cd0feb44c8c25c48b1f42c8
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-09-28 22:41:53 +0900

    :zap: :: add About SharedPreferences

--- app/src/main/java/com/guji/welfare/guji_welfare_e_android/data/MySharedPreferences.kt
+++ app/src/main/java/com/guji/welfare/guji_welfare_e_android/data/MySharedPreferences.kt
@@ -4,31 +4,54 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class MySharedPreferences(context: Context) {
-    companion object{
+    companion object {
         const val PREFS_FILENAME = "prefs"
-        const val PREF_KEY_ACCESSTOKEN = "accessToken"
-        const val PREF_KEY_MY_NAME = "name"
-        const val PREF_KEY_NICKNAME = "nickname"
-        const val PREF_KEY_PHONE_NUMBER = "phoneNumber"
-        const val PREF_KEY_DWELLING = "dwelling"
+
+        //Token
+        const val PREF_KEY_ACCESS_TOKEN = "accessToken"
+        const val PREF_KEY_REFRESH_TOKEN = "refreshToken"
+
+        //My
+        const val PREF_KEY_MY_NAME = "myName"
+        const val PREF_KEY_MY_NICKNAME = "myNickname"
+        const val PREF_KEY_MY_DWELLING = "myDwelling"
+
+        //WelfareWorker
+        const val PREF_KEY_WELFAREWORKER_PHONE_NUMBER = "welfareWorkerPhoneNumber"
+        const val PREF_KEY_WELFAREWORKER_NAME = "welfareWorkerName"
+        const val PREF_KEY_WELFAREWORKER_AFFILIATION = "welfareWorkerAffiliation"
     }
 
     private val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
 
+
     var accessToken: String?
-        get() = prefs.getString(PREF_KEY_ACCESSTOKEN, "")
-        set(value) = prefs.edit().putString(PREF_KEY_ACCESSTOKEN, value).apply()
+        get() = prefs.getString(PREF_KEY_ACCESS_TOKEN, "")
+        set(value) = prefs.edit().putString(PREF_KEY_ACCESS_TOKEN, value).apply()
+    var refreshToken: String?
+        get() = prefs.getString(PREF_KEY_REFRESH_TOKEN, "")
+        set(value) = prefs.edit().putString(PREF_KEY_REFRESH_TOKEN, value).apply()
+
+
     var myName: String?
         get() = prefs.getString(PREF_KEY_MY_NAME, "")
         set(value) = prefs.edit().putString(PREF_KEY_MY_NAME, value).apply()
-    var dwelling: String?
-        get() = prefs.getString(PREF_KEY_DWELLING, "")
-        set(value) = prefs.edit().putString(PREF_KEY_DWELLING, value).apply()
-    var nickname: String?
-        get() = prefs.getString(PREF_KEY_NICKNAME, "")
-        set(value) = prefs.edit().putString(PREF_KEY_NICKNAME, value).apply()
-    var phoneNumber: String?
-        get() = prefs.getString(PREF_KEY_PHONE_NUMBER, "")
-        set(value) = prefs.edit().putString(PREF_KEY_PHONE_NUMBER, value).apply()
+    var myDwelling: String?
+        get() = prefs.getString(PREF_KEY_MY_DWELLING, "")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_DWELLING, value).apply()
+    var myNickname: String?
+        get() = prefs.getString(PREF_KEY_MY_NICKNAME, "")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_NICKNAME, value).apply()
+
+
+    var welfareWorkerPhoneNumber: String?
+        get() = prefs.getString(PREF_KEY_WELFAREWORKER_PHONE_NUMBER, "")
+        set(value) = prefs.edit().putString(PREF_KEY_WELFAREWORKER_PHONE_NUMBER, value).apply()
+    var welfareWorkerName: String?
+        get() = prefs.getString(PREF_KEY_WELFAREWORKER_NAME, "")
+        set(value) = prefs.edit().putString(PREF_KEY_WELFAREWORKER_NAME, value).apply()
+    var welfareworkerAffiliation: String?
+        get() = prefs.getString(PREF_KEY_WELFAREWORKER_AFFILIATION, "")
+        set(value) = prefs.edit().putString(PREF_KEY_WELFAREWORKER_AFFILIATION, value).apply()
 
 }
