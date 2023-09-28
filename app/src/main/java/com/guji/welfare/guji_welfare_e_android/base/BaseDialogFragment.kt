f386340539cb763d45575c51097355ab124af89a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-09-28 22:32:15 +0900

    :zap: :: add BaseDialogFragment

---
+++ app/src/main/java/com/guji/welfare/guji_welfare_e_android/base/BaseDialogFragment.kt
@@ -0,0 +1,41 @@
+package com.guji.welfare.guji_welfare_e_android.base
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+
+abstract class BaseDialogFragment<B : ViewDataBinding, VM : BaseViewModel>(
+    @LayoutRes private val layoutRes: Int
+): DialogFragment() {
+
+    protected lateinit var viewModel: VM
+    protected lateinit var binding: B
+
+    abstract fun getViewModelClass(): Class<VM>
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        viewModel = ViewModelProvider(this)[getViewModelClass()]
+        binding.setVariable(layoutRes, viewModel)
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if(::binding.isInitialized)
+            binding.unbind()
+    }
+}
