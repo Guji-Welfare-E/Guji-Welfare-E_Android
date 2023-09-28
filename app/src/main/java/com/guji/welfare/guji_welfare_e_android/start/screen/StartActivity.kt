1f133ae9a2e2298b76e436c935913bf6eaedd59b
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-09-28 22:31:02 +0900

    :sparkles: :: add splash screen

--- app/src/main/java/com/guji/welfare/guji_welfare_e_android/start/screen/StartActivity.kt
+++ app/src/main/java/com/guji/welfare/guji_welfare_e_android/start/screen/StartActivity.kt
@@ -1,16 +1,29 @@
 package com.guji.welfare.guji_welfare_e_android.start.screen
 
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION
 import androidx.activity.viewModels
 import com.guji.welfare.guji_welfare_e_android.R
 import com.guji.welfare.guji_welfare_e_android.base.BaseActivity
-import com.guji.welfare.guji_welfare_e_android.databinding.ActivityMainBinding
 import com.guji.welfare.guji_welfare_e_android.databinding.ActivityStartBinding
+import com.guji.welfare.guji_welfare_e_android.main.screen.MainActivity
 import com.guji.welfare.guji_welfare_e_android.start.viewmodel.StartViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class StartActivity : BaseActivity<ActivityStartBinding, StartViewModel>(R.layout.activity_start) {
     override val viewModel: StartViewModel by viewModels()
 
     override fun start() {
-
+        CoroutineScope(Main).launch{
+            delay(1500)
+            Intent(this@StartActivity, MainActivity::class.java).also {
+                it.addFlags(FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(it)
+            }
+        }
     }
 }
