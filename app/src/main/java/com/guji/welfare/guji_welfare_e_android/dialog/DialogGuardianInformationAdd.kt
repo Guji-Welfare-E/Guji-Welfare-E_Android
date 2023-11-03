ba344075ab40bb38984fc18ce64e84b77508c66f
hamtory06 <ghkdwndhks@dgsw.hs.kr>
2023-11-03 20:49:40 +0900

    :zap: update DialogGuardian room db

--- app/src/main/java/com/guji/welfare/guji_welfare_e_android/dialog/DialogGuardianInformationAdd.kt
+++ app/src/main/java/com/guji/welfare/guji_welfare_e_android/dialog/DialogGuardianInformationAdd.kt
@@ -10,12 +10,20 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.guji.welfare.guji_welfare_e_android.R
 import com.guji.welfare.guji_welfare_e_android.base.BaseDialogFragment
+import com.guji.welfare.guji_welfare_e_android.data.room.AppDatabase
+import com.guji.welfare.guji_welfare_e_android.data.room.guardians.entity.Guardians
 import com.guji.welfare.guji_welfare_e_android.databinding.DialogGuardianInformationAddBinding
-import com.guji.welfare.guji_welfare_e_android.dialog.viewmodel.DialogGuardianInformationAddViewModel
+import com.guji.welfare.guji_welfare_e_android.main.adapter.data.GuardianInformationData
+import com.guji.welfare.guji_welfare_e_android.main.viewmodel.GuardianViewModel
+import com.guji.welfare.guji_welfare_e_android.util.NetworkManager
+import com.guji.welfare.guji_welfare_e_android.util.OnSingleClickListener
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
-class DialogGuardianInformationAdd :
-    BaseDialogFragment<DialogGuardianInformationAddBinding, DialogGuardianInformationAddViewModel>(
+class DialogGuardianInformationAdd(roomDB: AppDatabase?) :
+    BaseDialogFragment<DialogGuardianInformationAddBinding, GuardianViewModel>(
         R.layout.dialog_guardian_information_add
     ) {
 
@@ -24,57 +32,79 @@ class DialogGuardianInformationAdd :
     private lateinit var phoneNumber: String
     private lateinit var requestLauncher: ActivityResultLauncher<Intent>
 
-
-    override fun getViewModelClass(): Class<DialogGuardianInformationAddViewModel> =
-        DialogGuardianInformationAddViewModel::class.java
+    private val roomDB = roomDB
+    override fun getViewModelClass(): Class<GuardianViewModel> =
+        GuardianViewModel::class.java
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getPhoneData()
+        buttonEvent()
+    }
+
+    private fun buttonEvent(){
         with(binding) {
-            buttonYes.setOnClickListener {
+            buttonYes.setOnClickListener(OnSingleClickListener {
                 name = textGuardianName.text.toString()
                 relationship = textGuardianRelationship.text.toString()
                 phoneNumber = textGuardianPhoneNumber.text.toString()
+                if (name.isNotEmpty() && relationship.isNotEmpty() && phoneNumber.isNotEmpty()) {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        roomDB!!.guardiansDao()
+                            .insertItem(Guardians(0, name, phoneNumber, relationship))
+                    }
+                    val guardiansData = roomDB!!.guardiansDao().getAll()
+                    viewModel.updateGuardiansData(guardiansData.map {
+                        GuardianInformationData(it.name, it.telephoneNum, it.info)
+                    })
+                    if (NetworkManager.checkNetworkState(requireContext())) {
+                        val data = roomDB.guardiansDao().getAll()
+                        viewModel.updateGuardiansData(data.map {
+                            GuardianInformationData(it.name, it.info, it.telephoneNum)
+                        })
+                    }
+                    dismiss()
+                } else {
+                    Log.d("상태","name :$name, relationship :$relationship, phoneNumber: $phoneNumber")
+                }
+            })
+            buttonNo.setOnClickListener(OnSingleClickListener {
                 dismiss()
-            }
+            })
+        }
+    }
 
-            buttonNo.setOnClickListener {
-                dismiss()
-            }
+    private fun getPhoneData() {
+        binding.buttonFindPhoneNumber.setOnClickListener (OnSingleClickListener {
+            val intent =
+                Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            requestLauncher.launch(intent)
+        })
 
-            buttonFindPhoneNumber.setOnClickListener {
-                val intent =
-                    Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
-                requestLauncher.launch(intent)
-            }
+        requestLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    val cursor = requireActivity().contentResolver.query(
+                        it.data!!.data!!,
+                        arrayOf(
+                            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                            ContactsContract.CommonDataKinds.Phone.NUMBER,
+                        ),
+                        null,
+                        null,
+                        null
+                    )
 
-            requestLauncher =
-                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                    if (it.resultCode == RESULT_OK) {
-                        val cursor = requireActivity().contentResolver.query(
-                            it.data!!.data!!,
-                            arrayOf(
-                                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                                ContactsContract.CommonDataKinds.Phone.NUMBER,
-                            ),
-                            null,
-                            null,
-                            null
-                        )
-                        Log.d("test", "cursor size : ${cursor?.count}")
-
-                        if (cursor!!.moveToFirst()) {
-                            val name = cursor.getString(0)
-                            val phone = cursor.getString(1)
-                            with(binding) {
-                                textGuardianName.setText(name)
-                                textGuardianPhoneNumber.setText(phone)
-                            }
+                    if (cursor!!.moveToFirst()) {
+                        val name = cursor.getString(0)
+                        val phone = cursor.getString(1)
+                        with(binding) {
+                            textGuardianName.setText(name)
+                            textGuardianPhoneNumber.setText(phone)
                         }
                     }
                 }
-
-        }
+            }
     }
 
 }
