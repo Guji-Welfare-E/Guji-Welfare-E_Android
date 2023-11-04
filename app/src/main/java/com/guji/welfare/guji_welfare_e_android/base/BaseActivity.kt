6e34f0fa791dfe958d069e044dc6c5804a79106c
hamtory06 <ghkdwndhks@dgsw.hs.kr>
2023-11-04 20:48:34 +0900

    :zap: :: add viewModel

--- app/src/main/java/com/guji/welfare/guji_welfare_e_android/base/BaseActivity.kt
+++ app/src/main/java/com/guji/welfare/guji_welfare_e_android/base/BaseActivity.kt
@@ -5,14 +5,21 @@ import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
+import com.guji.welfare.guji_welfare_e_android.dialog.viewmodel.DiseaseViewModel
+import com.guji.welfare.guji_welfare_e_android.dialog.viewmodel.GuardianViewModel
 
 abstract class BaseActivity<B : ViewDataBinding, VM : BaseViewModel>(
     @LayoutRes private val layoutRes: Int
 ) : AppCompatActivity() {
 
     protected lateinit var binding: B
-    protected lateinit var mViewModel: VM
+    private lateinit var mViewModel: VM
     protected abstract val viewModel: VM
+
+    protected lateinit var diseaseViewModel: DiseaseViewModel
+    protected lateinit var guardianViewModel: GuardianViewModel
+
 
     protected open fun preLoad() {}
     protected abstract fun start()
@@ -27,10 +34,11 @@ abstract class BaseActivity<B : ViewDataBinding, VM : BaseViewModel>(
     private fun prepareDataBinding() {
         binding = DataBindingUtil.setContentView(this, layoutRes)
         mViewModel = if (::mViewModel.isInitialized) mViewModel else viewModel
+        diseaseViewModel = ViewModelProvider(this)[DiseaseViewModel::class.java]
+        guardianViewModel = ViewModelProvider(this)[GuardianViewModel::class.java]
 //        binding.setVariable(BR.vm, viewModel)
         binding.lifecycleOwner = this
         binding.executePendingBindings()
-
     }
 
     override fun onDestroy() {
