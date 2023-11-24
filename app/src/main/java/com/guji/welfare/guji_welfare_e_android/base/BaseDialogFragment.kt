f839f9490841f47f6729cea4553454440fedaa7c
hamtory06 <ghkdwndhks@dgsw.hs.kr>
2023-11-24 19:46:25 +0900

    :zap: :: fix dialog design

--- app/src/main/java/com/guji/welfare/guji_welfare_e_android/base/BaseDialogFragment.kt
+++ app/src/main/java/com/guji/welfare/guji_welfare_e_android/base/BaseDialogFragment.kt
@@ -1,9 +1,12 @@
 package com.guji.welfare.guji_welfare_e_android.base
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -27,6 +30,8 @@ abstract class BaseDialogFragment<B : ViewDataBinding, VM : ViewModel>(
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
         return binding.root
     }
 
